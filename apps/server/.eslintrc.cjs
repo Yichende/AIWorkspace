b7facd254e9f3b705af101215814dc7f7c101658
Yichend <957137116@qq.com>
2026-05-25 23:01:01 +0800

    refactor: remove .git; add frontend and backend

---
+++ apps/server/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+
+  parser: '@typescript-eslint/parser',
+
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+
+  plugins: ['@typescript-eslint', 'prettier'],
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+
+  env: {
+    node: true,
+    jest: true,
+  },
+
+  ignorePatterns: [
+    'dist',
+    'node_modules',
+  ],
+
+  rules: {
+    'prettier/prettier': 'warn',
+  },
+}
